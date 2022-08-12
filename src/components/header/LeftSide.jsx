7845a85850cee706c4b0cb8fbfaef8a9ab2c7693
Abolfazl <abolfazlm181920@gmail.com>
2022-08-12 17:53:45 +0430

    [responsive]: responsive header, logo, searchbar, profile!

--- src/components/header/LeftSide.jsx
+++ src/components/header/LeftSide.jsx
@@ -6,10 +6,8 @@ export const LeftSide = () => {
     return(
         <div>
             <img className={styles.ellogo} src={images.logo5} alt="logo" />
-            <div className={styles.bar}>
-                <input className={styles.searchbar} placeholder="search here" type="text" />
-                <img className={styles.sicon} src={images.icon8} />
-            </div>
+            <input className={styles.searchbar} placeholder="search here" type="text" />
+            <img className={styles.sicon} src={images.icon8} />
         </div>
     )
 }
