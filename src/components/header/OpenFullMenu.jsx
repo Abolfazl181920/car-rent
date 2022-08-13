dd427f81b3fe2bf428c4494d5ab78bdc30728500
Abolfazl <abolfazlm181920@gmail.com>
2022-08-13 15:41:37 +0430

    [bug]: resolve bug about hamberger menu!

---
+++ src/components/header/OpenFullMenu.jsx
@@ -0,0 +1,9 @@
+import styles from './Styles.module.css'
+
+export const OpenFullMenu = () => {
+    return (
+        <div className={styles.menu}>
+            open menu
+        </div>
+    )
+}
--- src/components/header/RightSide.jsx
+++ src/components/header/RightSide.jsx
@@ -1,14 +1,27 @@
+import { useState } from 'react'
+
 import { images } from '../../constants'
 
+import { OpenFullMenu } from './OpenFullMenu'
 import styles from './Styles.module.css'
 
 export const RightSide = () => {
+
+    const [ openMenu, setOpenMenu ] = useState(false)
+
+    const handleOpenMenu = () => {
+        setOpenMenu(!openMenu)
+        alert(openMenu)
+    }
+
     return(
-        <div className={styles.rightposition}>
-            <img className={styles.elrotate} src={images.icon10} alt="bing" />
-            <img className={styles.elrotate} src={images.icon7} alt="heart" />
-            <img className={styles.elrotate} src={images.icon12} alt="setting" />
-            <img className={styles.elrotate} src={images.logo7} alt="prfile" />
+        <div>
+            <div className={styles.rightposition}>
+                <img className={styles.elrotate} src={images.icon10} alt="bing" />
+                <img className={styles.elrotate} src={images.icon7} alt="heart" />
+                <img className={styles.elrotate} src={images.icon12} alt="setting" />
+                <img onClick={handleOpenMenu} className={styles.elrotate} src={images.logo7} alt="prfile" />
+            </div>
         </div>
     )
 }
