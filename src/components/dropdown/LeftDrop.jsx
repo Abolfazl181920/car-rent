f9b866a8e115e313fe5e0ef67d70c57cd3410f07
Abolfazl <abolfazlm181920@gmail.com>
2022-08-20 20:42:34 +0430

    [package-test]: tested react-dropdown package and worked!

--- src/components/dropdown/LeftDrop.jsx
+++ src/components/dropdown/LeftDrop.jsx
@@ -1,7 +1,20 @@
+import Dropdown from 'react-dropdown'
+import 'react-dropdown/style.css'
+
+
 export const LeftDrop = () => {
+
+    const options = [
+        'one', 'two', 'three'
+      ];
+    const defaultOption = options[0];
+
+    const onSelect = () => {}
+
     return(
         <div>
-            LeftDrop
+            <h1>LeftDrop</h1>
+            <Dropdown options={options} onChange={onSelect} value={defaultOption} placeholder="Select an option" />;
         </div>
     )
 }
